cb674aa1c8e4fd3a09b7b9689e5302efcc0be4bd
TashfeenRaza <125599332+TashfeenRaza@users.noreply.github.com>
2023-02-17 19:51:06 +0500

    Add files via upload

---
+++ Personal_Message.js
@@ -0,0 +1,6 @@
+// Q2- Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+let myName = "Tashfeen Raza";
+
+/* Printing the message in the console. */
+console.log(`Hello ${myName}, would you like to learn some javascript today?`);
